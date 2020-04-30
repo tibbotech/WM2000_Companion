e38e9f51e166eb9bf4f9f3da9c30b0828f58578a
Hsiang Chou <hsiang@tibbo.com>
2020-04-30 14:12:33 +0800

    Initial commit. Left placeholders for Luis to add new syscalls

---
+++ Platforms/Configurators/CODY_configurator/main.js
@@ -0,0 +1,38 @@
+
+//.............................................................................
+function cleanArray(actual) {
+    var newArray = new Array();
+    var tempstr = new String;
+
+    for (var i = 0; i < actual.length; i++) {
+        if (actual[i]) {
+            newArray.push(actual[i]);
+        }
+    }
+
+    return newArray;
+}
+//.............................................................................
+function Decode(Text) {
+    return Text;
+}
+
+function ParseLine(Line) {
+    return Line;
+}
+
+function ParseOptions(s) {
+    return s;
+}
+
+//.............................................................................
+
+function Encode(XText) {
+    return XText;
+}
+
+function FormatLine(str) {
+    return str;
+}
+
+  //.............................................................................
